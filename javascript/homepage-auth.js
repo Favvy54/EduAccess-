b196c5b2a753e561656ea88757e168bdd0121376
Favvy54 <favourangel884@gmail.com>
2026-02-19 00:09:56 +0100

    Resolved conflicted

--- javascript/homepage-auth.js
+++ javascript/homepage-auth.js
@@ -1,6 +1,5 @@
-// ============================================
+
 // FIREBASE IMPORTS
-// ============================================
 
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js';
 import { getAuth, onAuthStateChanged } from 'https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js';
